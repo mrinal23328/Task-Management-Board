6b9e652c9b42aac09011bfea805b2794e67f48ef
mrinal23328 <mrinal23328@iiitd.ac.in>
2025-05-19 18:58:16 +0530

    Updated deployment settings

--- tasks/deployment_settings.py
+++ tasks/deployment_settings.py
@@ -37,11 +37,7 @@ STORAGES = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
