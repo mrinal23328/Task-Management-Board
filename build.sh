12fb176fdd0c879a9784d1f5536bc385eb8cb7fa
mrinal23328 <mrinal23328@iiitd.ac.in>
2025-05-19 19:16:07 +0530

    Updated deployment settings

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
+export DATABASE_PATH=/opt/render/project/src/db/db.sqlite3
+
 python manage.py collectstatic --no-input
 
 python manage.py makemigrations
