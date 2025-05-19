9a2f855d318e1d004d377a2464c6dc3b93baee58
mrinal23328 <mrinal23328@iiitd.ac.in>
2025-05-19 18:49:20 +0530

    Updated deployment settings

--- tasks/deployment_settings.py
+++ tasks/deployment_settings.py
@@ -36,9 +36,12 @@ STORAGES = {
 }
 
 DATABASES = {
-    'default': dj_database_url.config(
-        env=os.environ['DATABASE_URL'],
-        engine='django.db.backends.postgresql',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT', '5432'),
+    }
 }
